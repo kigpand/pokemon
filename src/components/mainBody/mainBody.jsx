146f7f438bb5701baf5898ff63814f29853558e5
kigpand <kigpand@naver.com>
2022-09-26 21:29:36 +0900

    인피니티 스크롤링 구현

--- src/components/mainBody/mainBody.jsx
+++ src/components/mainBody/mainBody.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
@@ -9,11 +10,14 @@ const MainBody = () => {
     const [data, setData] = useState([]);
     const pokemonList = useSelector((state) => state.pokemon.pokemonList);
     const generate = useSelector((state) => state.pokemon.generate);
+    const [bottom, setBottom] = useState(null);
+    const bottomOb= useRef();
+    let num = 10;
 
     useEffect(() => {
         if (generate !== null) {
             if (generate === 'all') {
-                return setData(pokemonList.slice(370, 380));
+                return setData(pokemonList.slice(0, 10));
             } else {
                 // const array = dummy.filter((item) => item.generate === generate);
                 // setData(array);
@@ -21,11 +25,39 @@ const MainBody = () => {
         }
     }, [generate, pokemonList]);
 
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            entries => {
+                if (entries[0].isIntersecting) {
+                    // eslint-disable-next-line react-hooks/exhaustive-deps
+                    num += 10;
+                    setData(pokemonList.slice(0, num));
+                }
+            }
+        );
+        bottomOb.current = observer;
+    }, [pokemonList, data]);
+
+    useEffect(() => {
+        const observer = bottomOb.current;
+        if (bottom) {
+            observer.observe(bottom);
+        }
+        return () => {
+            if (bottom) {
+                observer.unobserve(bottom);
+            }
+        }
+    }, [bottom]);
+
     return(
         <div className={styles.mainBody}>
-            {data.map((data) => {
-                return <PokemonList pokemon={data} key={data.name}/>; 
-            })}
+            <div className={styles.lists}>
+                {data.map((data) => {
+                    return <PokemonList pokemon={data} key={data.name}/>; 
+                })}
+            </div>
+            <div className={styles.bottom} ref={setBottom}>bottom</div>
         </div>
     )
 }
