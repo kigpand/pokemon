54836fb567522bfa9671c4b19d2f1fc4a0747cbc
kigpand <kigpand@naver.com>
2022-10-17 23:09:44 +0900

    type ui 수정

--- src/container/Type/Type.tsx
+++ src/container/Type/Type.tsx
@@ -3,9 +3,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { ITypeKoData } from '../../interface/IPokemonList';
 import { setTypeLists } from '../../reducers/datas';
 import { RootState } from '../../store/store';
-import { getTypeConvertData, getTypeKo } from '../../utils/convert';
+import { getColor, getTypeConvertData, getTypeKo } from '../../utils/convert';
 import { getTypes } from '../../utils/network';
 import styles from './Type.module.scss';
+import { useCallback } from 'react';
 
 interface ITypeData {
     id: number;
@@ -36,13 +37,13 @@ const Type = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [currentType]);
 
-    async function getTypeData() {
+    const getTypeData = useCallback(async () => {
         if (currentType) {
             await getTypes(currentType)
             .then((data: ITypeData[]) => {
                 const typeList: ITypeKoData = {
                     id: data[0].id,
-                    name: getTypeKo(data[0].name),
+                    name: data[0].name,
                     doubleDamegeFrom: getTypeConvertData(data[0].doubleDamegeFrom),
                     doubleDamegeTo: getTypeConvertData(data[0].doubleDamegeTo),
                     halfDamegeFrom: getTypeConvertData(data[0].halfDamegeFrom),
@@ -54,11 +55,67 @@ const Type = () => {
                 setType(typeList);
             });
         }
-    }
+    }, [currentType, dispatch]);
+
+    useEffect(() => {
+        console.log(type);
+    }, [type]);
 
     return (
         <div className={styles.type}>
-            {type?.name}
+            { type && 
+                <div className={styles.container} style={{ backgroundColor: getColor(type.name)}}>
+                    {getTypeKo(type?.name)}
+                    <div className={styles.doubleDamegeFrom}>
+                        <div className={styles.title}>2배 데미지</div>
+                        <div className={styles.items}>
+                            {type.doubleDamegeFrom?.map((item: string, i: number) => {
+                                return <span key={i}>{item}</span>
+                            })}
+                        </div>
+                    </div>
+                    <div className={styles.doubleDamegeTo}>
+                        <div className={styles.title}>2배 준다</div>
+                        <div className={styles.items}>
+                            {type.doubleDamegeTo?.map((item: string, i: number) => {
+                                return <span key={i}>{item}</span>
+                            })}
+                        </div>
+                    </div>
+                    <div className={styles.halfDamegeFrom}>
+                        <div className={styles.title}>절반 데미지</div>
+                        <div className={styles.items}>
+                            {type.halfDamegeFrom?.map((item: string, i: number) => {
+                                return <span key={i}>{item}</span>
+                            })}
+                        </div>
+                    </div>
+                    <div className={styles.halfDamegeTo}>
+                        <div className={styles.title}>절반 데미지를 준다</div>
+                        <div className={styles.items}>
+                            {type.halfDamegeTo?.map((item: string, i: number) => {
+                                return <span key={i}>{item}</span>
+                            })}
+                        </div>
+                    </div>
+                    <div className={styles.noDamegeFrom}>
+                        <div className={styles.title}>데미지를 줄수 없다</div>
+                        <div className={styles.items}>
+                            {type.noDamegeFrom?.map((item: string, i: number) => {
+                                return <span key={i}>{item}</span>
+                            })}
+                        </div>
+                    </div>
+                    <div className={styles.noDamegeTo}>
+                        <div className={styles.title}>데미지를 받지 않는다</div>
+                        <div className={styles.items}>
+                            {type.noDamegeTo?.map((item: string, i: number) => {
+                                return <span key={i}>{item}</span>
+                            })}
+                        </div>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,22 +1,22 @@
 export function getColor(type: string) {
-    if (type === 'water') return '#5ec4ff';
-    if (type === 'grass') return '#3aff6b';
-    if (type === 'poison') return '#b639ff';
-    if (type === 'fire') return '#ff0000';
-    if (type === 'normal') return '#888888';
-    if (type === 'electric') return '#fff34a';
-    if (type === 'ice') return '#42f9ff';
-    if (type === 'fighting') return '#b15429';
-    if (type === 'ground') return '#b46c00';
-    if (type === 'flying') return '#1f66ff';
-    if (type === 'psychic') return '#fa51b3';
-    if (type === 'bug') return '#257439';
-    if (type === 'rock') return '#977f13';
-    if (type === 'ghost') return '#5e379e';
-    if (type === 'dragon') return '#4251aa';
-    if (type === 'dark') return '#584a30';
-    if (type === 'steel') return '#797979';
-    if (type === 'fairy') return '#ff35b2';
+    if (type === 'water' || type === '물') return '#5ec4ff';
+    if (type === 'grass' || type === '풀') return '#3aff6b';
+    if (type === 'poison' || type === '독') return '#b639ff';
+    if (type === 'fire' || type === '불') return '#ff0000';
+    if (type === 'normal' || type === '노말') return '#888888';
+    if (type === 'electric' || type === '전기') return '#fff34a';
+    if (type === 'ice' || type === '얼음') return '#42f9ff';
+    if (type === 'fighting' || type === '격투') return '#b15429';
+    if (type === 'ground' || type === '땅') return '#b46c00';
+    if (type === 'flying' || type === '비행') return '#1f66ff';
+    if (type === 'psychic' || type === '에스퍼') return '#fa51b3';
+    if (type === 'bug' || type === '벌레') return '#257439';
+    if (type === 'rock' || type === '바위') return '#977f13';
+    if (type === 'ghost' || type === '고스트') return '#5e379e';
+    if (type === 'dragon' || type === '드래곤') return '#4251aa';
+    if (type === 'dark' || type === '악') return '#584a30';
+    if (type === 'steel' || type === '강철') return '#797979';
+    if (type === 'fairy' || type === '페어리') return '#ff35b2';
 }
 
 export function getTypeKo(type: string) {
