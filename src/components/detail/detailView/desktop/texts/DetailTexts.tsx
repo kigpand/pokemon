103a7385188b2ee1f4032940d38aca5561ea64bf
김지훈 <kigpand@gmail.com>
2023-05-06 23:34:08 +0900

    detail text component 분리

--- src/components/detail/detailView/desktop/texts/DetailTexts.tsx
+++ src/components/detail/detailView/desktop/texts/DetailTexts.tsx
@@ -1,14 +1,10 @@
 import { useDispatch } from "react-redux";
 import { IPokemonList } from "../../../../../interface/IPokemonList";
-import {
-  getStatusBarColor,
-  getTypeIcon,
-  getTypeKo,
-} from "../../../../../utils/convert";
 import styles from "./DetailTexts.module.scss";
 import { setCurrentAbility } from "../../../../../reducers/datas";
 import { useNavigate } from "react-router-dom";
 import React from "react";
+import TextItem from "./textItem/TextItem";
 
 interface IDetailTexts {
   currentPoke: IPokemonList;
@@ -35,62 +31,10 @@ const DetailTexts = ({ currentPoke }: IDetailTexts) => {
         <div className={styles.generate}>{currentPoke!.generate}</div>
       </div>
       <div className={styles.lists}>
-        <div className={styles.list}>
-          <div className={styles.title}>분류</div>
-          <div className={styles.body}>{currentPoke!.genus}</div>
-        </div>
-        <div className={styles.list}>
-          <div className={styles.title}>타입</div>
-          <div className={styles.body}>
-            {currentPoke!.types?.map((item: string, i: number) => {
-              return (
-                <div
-                  className={styles.type}
-                  key={i}
-                  onClick={() => onTypeClick(item)}
-                >
-                  <img
-                    src={`${process.env.PUBLIC_URL}/${getTypeIcon(item)}`}
-                    alt="img"
-                    className={styles.icon}
-                  />
-                  <div className={styles.item}>{getTypeKo(item)}</div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        <div className={styles.list}>
-          <div className={styles.title}>특성</div>
-          <div className={styles.body}>
-            {currentPoke!.abilities?.map((item: string, i: number) => {
-              return (
-                <div className={styles.ability} key={i}>
-                  <div className={styles.item} onClick={() => onAbility(item)}>
-                    {item}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        <div className={styles.list}>
-          <div className={styles.title}>종족값</div>
-          <div className={styles.body}>
-            {currentPoke!.stats?.map((item: any, i: number) => {
-              return (
-                <div
-                  className={styles.stat}
-                  style={{ backgroundColor: getStatusBarColor(item.name) }}
-                  key={i}
-                >
-                  <div className={styles.item}>{item.name}</div>
-                  <div className={styles.value}>{item.stat}</div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
+        <TextItem items={currentPoke} type="분류" />
+        <TextItem items={currentPoke} type="타입" onClick={onTypeClick} />
+        <TextItem items={currentPoke} type="특성" onClick={onAbility} />
+        <TextItem items={currentPoke} type="종족값" />
       </div>
     </div>
   );
---
+++ src/components/detail/detailView/desktop/texts/textItem/TextItem.tsx
@@ -0,0 +1,83 @@
+import { IPokemonList } from "../../../../../../interface/IPokemonList";
+import {
+  getStatusBarColor,
+  getTypeIcon,
+  getTypeKo,
+} from "../../../../../../utils/convert";
+import styles from "./TextItem.module.scss";
+
+interface ITextItem {
+  type: string;
+  items: IPokemonList;
+  onClick?: (item: any) => void;
+}
+
+function getRenderItem(items: IPokemonList, type: string, onClick: any) {
+  switch (type) {
+    case "타입":
+      return (
+        <div className={styles.body}>
+          {items.types?.map((item: string, i: number) => {
+            return (
+              <div
+                className={styles.type}
+                key={i}
+                onClick={() => onClick(item)}
+              >
+                <img
+                  src={`${process.env.PUBLIC_URL}/${getTypeIcon(item)}`}
+                  alt="img"
+                  className={styles.icon}
+                />
+                <div className={styles.item}>{getTypeKo(item)}</div>
+              </div>
+            );
+          })}
+        </div>
+      );
+    case "특성":
+      return (
+        <div className={styles.body}>
+          {items.abilities?.map((item: string, i: number) => {
+            return (
+              <div className={styles.ability} key={i}>
+                <div className={styles.item} onClick={() => onClick(item)}>
+                  {item}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      );
+    case "종족값":
+      return (
+        <div className={styles.body}>
+          {items.stats?.map((item: any, i: number) => {
+            return (
+              <div
+                className={styles.stat}
+                style={{ backgroundColor: getStatusBarColor(item.name) }}
+                key={i}
+              >
+                <div className={styles.item}>{item.name}</div>
+                <div className={styles.value}>{item.stat}</div>
+              </div>
+            );
+          })}
+        </div>
+      );
+    default:
+      return <div className={styles.body}>{items.genus}</div>;
+  }
+}
+
+const TextItem = ({ type, items, onClick }: ITextItem) => {
+  return (
+    <div className={styles.textItem}>
+      <div className={styles.title}>{type}</div>
+      {getRenderItem(items, type, onClick)}
+    </div>
+  );
+};
+
+export default TextItem;
