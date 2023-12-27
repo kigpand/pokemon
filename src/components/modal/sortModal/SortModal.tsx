2595699512817e43a7a2240f930146c750a8e34c
김지훈 <kigpand@gmail.com>
2023-12-27 22:09:28 +0900

    dark theme 지정 안된 컴포넌트 수정

--- src/components/modal/sortModal/SortModal.tsx
+++ src/components/modal/sortModal/SortModal.tsx
@@ -2,11 +2,12 @@ import "../../../common/event.scss";
 import styles from "./SortModal.module.scss";
 import { typeList, geneList } from "./sort";
 import pokeData from "../../../json/pokemonList.json";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { resetCurrentList, setPokemonList } from "../../../reducers/pokemon";
 import { convertPokeData } from "../../../utils/makeData";
 import SortSelect from "./select/SortSelect";
 import SortItem from "./item/SortItem";
+import { RootState } from "../../../store/store";
 
 interface ISortModal {
   closeSort: () => void;
@@ -14,6 +15,7 @@ interface ISortModal {
 
 const SortModal = ({ closeSort }: ISortModal) => {
   const dispatch = useDispatch();
+  const theme = useSelector((state: RootState) => state.datas.theme);
 
   function onCloseBtn() {
     closeSort();
@@ -27,7 +29,12 @@ const SortModal = ({ closeSort }: ISortModal) => {
   }
 
   return (
-    <div className={styles.sortModal}>
+    <div
+      className={styles.sortModal}
+      style={{
+        backgroundColor: theme === "dark" ? "black" : "white",
+      }}
+    >
       <div className={styles.sortBtn} onClick={onCloseBtn}>
         닫기
       </div>
--- src/components/modal/typeModal/TypeModal.tsx
+++ src/components/modal/typeModal/TypeModal.tsx
@@ -2,6 +2,8 @@ import styles from "./TypeModal.module.scss";
 import types from "../../../json/types.json";
 import { getTypeIcon, getTypeKo } from "../../../utils/convert";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store/store";
 
 type Props = {
   onCloseModal: () => void;
@@ -9,6 +11,7 @@ type Props = {
 
 export default function TypeModal({ onCloseModal }: Props) {
   const nav = useNavigate();
+  const theme = useSelector((state: RootState) => state.datas.theme);
 
   function onType(type: string) {
     onCloseModal();
@@ -18,7 +21,13 @@ export default function TypeModal({ onCloseModal }: Props) {
 
   return (
     <div className={styles.typeModal}>
-      <div className={styles.main}>
+      <div
+        className={styles.main}
+        style={{
+          backgroundColor: theme === "dark" ? "black" : "white",
+          color: theme === "dark" ? "white" : "black",
+        }}
+      >
         <div className={styles.title}>타입을 선택해주세요</div>
         <div className={styles.line}></div>
         <div className={styles.content}>
@@ -33,6 +42,7 @@ export default function TypeModal({ onCloseModal }: Props) {
                 <img
                   src={getTypeIcon(item.name)}
                   className={styles.types}
+                  alt={item.name}
                 ></img>
               </div>
             );
