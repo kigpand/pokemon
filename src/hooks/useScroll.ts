be181f2b23302d5040a26285f4ec6b927911e967
김지훈 <kigpand@gmail.com>
2024-10-27 20:18:07 +0900

    불필요한 useEffect 구문 제거

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,14 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setPokemonList } from "reducers/pokemon";
-import { convertPokeData } from "utils/makeData";
-import pokeData from "json/pokemonList.json";
-import { RootState } from "store/store";
 
 export function useScroll() {
   const [scroll, setScroll] = useState<number>(0);
-  const dispatch = useDispatch();
-  const { currentList } = useSelector((state: RootState) => state.pokemon);
 
   const onScroll = useCallback(() => {
     if (
@@ -20,16 +13,12 @@ export function useScroll() {
   }, []);
 
   useEffect(() => {
-    if (currentList.length === 0) {
-      const list = convertPokeData(pokeData);
-      dispatch(setPokemonList(list));
-    }
     window.addEventListener("scroll", onScroll);
 
     return () => {
       window.removeEventListener("scroll", onScroll);
     };
-  }, []);
+  }, [onScroll]);
 
   return { scroll };
 }
--- src/reducers/pokemon.ts
+++ src/reducers/pokemon.ts
@@ -1,43 +1,55 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { IPokemonList } from '../interface/IPokemonList';
+import { createSlice } from "@reduxjs/toolkit";
+import { IPokemonList } from "../interface/IPokemonList";
+import { convertPokeData } from "utils/makeData";
+import pokeData from "json/pokemonList.json";
 
 interface InitState {
-    pokemonList: IPokemonList[],
-    currentList: IPokemonList[],
-    bookPokeList: IPokemonList[]
+  pokemonList: IPokemonList[];
+  currentList: IPokemonList[];
+  bookPokeList: IPokemonList[];
 }
 
 const initialState: InitState = {
-    pokemonList: [],
-    currentList: [],
-    bookPokeList: []
-}
+  pokemonList: convertPokeData(pokeData),
+  currentList: [],
+  bookPokeList: [],
+};
 
 export const counterSlice = createSlice({
-    name: 'pokemon',
-    initialState,
-    reducers: {
-        setPokemonList: (state, action) => {
-            state.pokemonList = action.payload;
-        },
-        setCurrentList: (state, action) => {
-            state.currentList = [...state.currentList, ...action.payload];
-        },
-        resetCurrentList: (state, action) => {
-            state.currentList = action.payload;
-        },
-        setBookPokeList: (state, action) => {
-            if (state.bookPokeList.length > 5) {
-                state.bookPokeList.shift();
-            }
-            state.bookPokeList.push(action.payload);
-        },
-        removeBookPokeList: (state, action) => {
-            state.bookPokeList = [...state.bookPokeList.filter((list: IPokemonList) => list.id !== action.payload.list.id)];
-        }
+  name: "pokemon",
+  initialState,
+  reducers: {
+    setPokemonList: (state, action) => {
+      state.pokemonList = action.payload;
     },
-})
+    setCurrentList: (state, action) => {
+      state.currentList = [...state.currentList, ...action.payload];
+    },
+    resetCurrentList: (state, action) => {
+      state.currentList = action.payload;
+    },
+    setBookPokeList: (state, action) => {
+      if (state.bookPokeList.length > 5) {
+        state.bookPokeList.shift();
+      }
+      state.bookPokeList.push(action.payload);
+    },
+    removeBookPokeList: (state, action) => {
+      state.bookPokeList = [
+        ...state.bookPokeList.filter(
+          (list: IPokemonList) => list.id !== action.payload.list.id
+        ),
+      ];
+    },
+  },
+});
 
-export const { setPokemonList, setBookPokeList, removeBookPokeList, setCurrentList, resetCurrentList } = counterSlice.actions
+export const {
+  setPokemonList,
+  setBookPokeList,
+  removeBookPokeList,
+  setCurrentList,
+  resetCurrentList,
+} = counterSlice.actions;
 
 export default counterSlice.reducer;
